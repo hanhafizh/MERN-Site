63129cf60d887fa86ab8349fcdd0947a677563aa
hanhafizh <farhanhafizh770@gmail.com>
2023-04-19 20:12:30 +0700

    Membuat Styling Create Blog Post

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -2,5 +2,7 @@ import Button from "./Button";
 import Input from "./input";
 import Gap from "./Gap";
 import Link from "./Link";
+import Upload from "./Upload";
+import TextArea from "./TextArea";
 
-export { Input, Button, Gap, Link };
+export { Input, Button, Gap, Link, Upload, TextArea };
